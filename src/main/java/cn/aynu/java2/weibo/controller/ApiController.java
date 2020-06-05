1d847359b44bffe16ee29dbf0cd5aae4399c2687
涵 田 <892265525@qq.com>
2020-06-05 16:12:10 +0800

    实现删除动态 首页头像和基本信息注入 实现根据用户id查询动态（前端未完成） 完善上传文件失败情况下的回滚与文件重新删除

---
+++ src/main/java/cn/aynu/java2/weibo/controller/ApiController.java
@@ -0,0 +1,38 @@
+package cn.aynu.java2.weibo.controller;
+
+import cn.aynu.java2.weibo.entity.Result;
+import cn.aynu.java2.weibo.entity.User;
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ * @author tianh
+ */
+@RestController
+@RequestMapping("/main/api")
+public class ApiController {
+    @GetMapping("/LoginUser")
+    public JSONObject getUser(HttpSession s){
+        JSONObject json=new JSONObject();
+        try{
+            User user= (User) s.getAttribute("login_user");
+            if(user!=null){
+                user.setPassword(null);
+                user.setBirthday(null);
+                user.setRegisterDay(null);
+                user.setEmail(null);
+                json.put("result", Result.successResult("获取登录态成功!",user));
+            }else{
+                json.put("result",Result.failResult("获取登录态失败,请重新登陆"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            json.put("result", Result.exceptionResult(e.getMessage()));
+        }
+        return json;
+    }
+}
