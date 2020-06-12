7d01a99ce37c3068405edc0aea198a9f4d7f9f87
涵 田 <892265525@qq.com>
2020-06-12 19:48:27 +0800

    合并配置类

--- src/main/java/cn/aynu/java2/weibo/config/WebMvcConfig.java
+++ src/main/java/cn/aynu/java2/weibo/config/WebMvcConfig.java
@@ -1,5 +1,6 @@
 package cn.aynu.java2.weibo.config;
 
+import cn.aynu.java2.weibo.utils.LoginInterceptor;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -28,5 +30,16 @@ public class WebMvcConfig implements WebMvcConfigurer {
         registry.addViewController("/test").setViewName("/system/mem_list");
         registry.addViewController("/main").setViewName("main");
         registry.addViewController("/connection").setViewName("connection");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor())
+                .addPathPatterns("/**")
+                .excludePathPatterns("/**/*.html",
+                        "/**/*.js",              //js静态资源
+                        "/**/*.css",             //css静态资源
+                        "/**/*.woff",
+                        "/**/*.ttf");
     }
 }
