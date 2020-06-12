a52cf2f03455d724c446bf66bdb1331dcd83665f
涵 田 <892265525@qq.com>
2020-06-12 12:50:43 +0800

    完成我的关注界面（共同关注好友） 完成评论删除功能

--- src/main/java/cn/aynu/java2/weibo/controller/ApiController.java
+++ src/main/java/cn/aynu/java2/weibo/controller/ApiController.java
@@ -4,13 +4,19 @@ import cn.aynu.java2.weibo.entity.Result;
 import cn.aynu.java2.weibo.entity.User;
 import cn.aynu.java2.weibo.service.IUserService;
 import cn.aynu.java2.weibo.utils.VoUtils;
+import cn.aynu.java2.weibo.vo.ConnectionVo;
 import com.alibaba.fastjson.JSONObject;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 import static cn.aynu.java2.weibo.entity.Result.*;
 
@@ -24,6 +30,43 @@ public class ApiController {
     VoUtils voUtils;
     @Resource
     IUserService userService;
+    @Resource(name = "redisTemplate")
+    RedisTemplate<Object,Object> redisTemplate;
+
+    @GetMapping("/gzConnection")
+    public JSONObject getGzConnection(HttpSession s){
+        JSONObject json=new JSONObject();
+        List<ConnectionVo> vos=new ArrayList<>();
+        try{
+            User user= (User) s.getAttribute("login_user");
+            Set<Object> members = redisTemplate.opsForSet().members("gz:userId:" + user.getId());
+            if(members.size()>1){
+               vos = voUtils.transferToConnectionVo(members, user);
+            }
+            json.put("result",successResult("查找成功",vos));
+        } catch (Exception e) {
+            e.printStackTrace();
+            json.put("result", exceptionResult(e.getMessage()));
+        }
+        return json;
+    }
+    @GetMapping("/fsConnection")
+    public JSONObject getFsConnection(HttpSession s){
+        JSONObject json=new JSONObject();
+        List<ConnectionVo> vos=new ArrayList<>();
+        try{
+            User user= (User) s.getAttribute("login_user");
+            Set<Object> members = redisTemplate.opsForSet().members("fs:userId:" + user.getId());
+            if(members.size()>1){
+                vos = voUtils.transferToConnectionVo(members, user);
+            }
+            json.put("result",successResult("查找成功",vos));
+        } catch (Exception e) {
+            e.printStackTrace();
+            json.put("result", exceptionResult(e.getMessage()));
+        }
+        return json;
+    }
     @GetMapping("/LoginUser")
     public JSONObject getUser(HttpSession s){
         JSONObject json=new JSONObject();
--- src/main/java/cn/aynu/java2/weibo/vo/ConnectionVo.java
+++ src/main/java/cn/aynu/java2/weibo/vo/ConnectionVo.java
@@ -2,19 +2,47 @@ package cn.aynu.java2.weibo.vo;
 
 import cn.aynu.java2.weibo.entity.User;
 
+import java.util.List;
+
 /**
  * @author tianh
  */
 public class ConnectionVo {
     private User user;
     private Integer commonFriend;
+    private List<User> commonFriends;
+    private boolean friend;
+    public ConnectionVo() {
+    }
 
     @Override
     public String toString() {
         return "ConnectionVo{" +
                 "user=" + user +
                 ", commonFriend=" + commonFriend +
+                ", commonFriends=" + commonFriends +
+                ", friend=" + friend +
                 '}';
+    }
+
+    public boolean isFriend() {
+        return friend;
+    }
+
+    public void setFriend(boolean friend) {
+        this.friend = friend;
+    }
+
+    public List<User> getCommonFriends() {
+        return commonFriends;
+    }
+
+    public void setCommonFriends(List<User> commonFriends) {
+        this.commonFriends = commonFriends;
+    }
+
+    public ConnectionVo(User user) {
+        this.user = user;
     }
 
     public User getUser() {
